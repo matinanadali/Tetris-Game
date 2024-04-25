62f48892a67a6869e43252b19ba1d6a1e8fcc7cb
matinandali <matinanad@gmail.com>
2024-04-25 11:05:02 +0300

    added sound

--- programs/game/interface.c
+++ programs/game/interface.c
@@ -6,11 +6,15 @@
 #include <math.h>
 #include <stdio.h>
 
-// Texture spaceship_img;
+Music music;
+
 void interface_init() {
 	// Αρχικοποίηση του παραθύρου
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "game");
 	SetTargetFPS(60);
+    InitAudioDevice();    
+    music = LoadMusicStream("assets/cover.mp3");
+    PlayMusicStream(music);
 }
 
 void interface_close() {
@@ -23,18 +27,18 @@ bool is_zero(double a) {
 }
 
 void draw_triangle(Vector2 pointA, Vector2 pointB, Vector2 pointC) {
-   double slope1 = (pointB.y - pointA.y) * (pointC.x - pointB.x);
-double slope2 = (pointC.y - pointB.y) * (pointB.x - pointA.x);
-
+    double slope1 = (pointB.y - pointA.y) * (pointC.x - pointB.x);
+    double slope2 = (pointC.y - pointB.y) * (pointB.x - pointA.x);
     if (slope1 > slope2) {
         DrawTriangleLines(pointA, pointB, pointC, WHITE);
     }
     else {
         DrawTriangleLines(pointA, pointC, pointB, WHITE);
     }
- 
-}
+} 
+
 void interface_draw_frame(State state) {
+    UpdateMusicStream(music);
     BeginDrawing();
 
 	// Καθαρισμός, θα τα σχεδιάσουμε όλα από την αρχή
@@ -43,15 +47,13 @@ void interface_draw_frame(State state) {
     int height = GetScreenHeight();
 
 	// Σχεδιάζουμε το διαστημόπλοιο
-  //  Vector2 new_origin = (Vector2){+width / 2-state_info(state)->spaceship->position.x, +height / 2-state_info(state)->spaceship->position.y};
+
     double angle = atan2(state_info(state)->spaceship->orientation.y, state_info(state)->spaceship->orientation.x);
 
     Vector2 point = {0, -40};
     Vector2 pointB = {20, 0};
     Vector2 pointC = {-20,0};
     
-     
-
     point = vec2_rotate(point, -angle + PI / 2);
     pointB = vec2_rotate(pointB, -angle + PI / 2);
     pointC = vec2_rotate(pointC, -angle + PI / 2);
@@ -62,8 +64,9 @@ void interface_draw_frame(State state) {
   
     
     draw_triangle(pointB, pointC, point);
-   double sx = state_info(state)->spaceship->position.x;
+    double sx = state_info(state)->spaceship->position.x;
     double sy = state_info(state)->spaceship->position.y;
+   
     // Σχεδιάζουμε τα υπόλοιπα αντικείμενα (σφαίρες και αστεροειδείς)
     Vector2 top_left = (Vector2){-width/2+sx, sy+height/2};
     Vector2 bottom_right = (Vector2){sx+width/2, -height/2+sy};
@@ -83,6 +86,6 @@ void interface_draw_frame(State state) {
     DrawText(TextFormat("%04i", state_info(state)->score), 20, 20, 40, GRAY);
 	DrawFPS(SCREEN_WIDTH - 80, 0);
 	
-   EndDrawing();
+    EndDrawing();
 }
 
